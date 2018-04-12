0ba121d9490b005c175f81eac74fa83ef89a3ee5
cobaltqirin <imquyen@live.com.au>
2018-04-12 23:34:52 +1000

    remove wordpress, use template

--- index.php
+++ index.php
@@ -1,17 +1,57 @@
-<?php
-/**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
- *
- * @package WordPress
- */
+<?php get_header(); ?>
 
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define('WP_USE_THEMES', true);
-
-/** Loads the WordPress Environment and Template */
-require( dirname( __FILE__ ) . '/wp-blog-header.php' );
+<div class="content">
+																	                    
+	<?php if (have_posts()) : ?>
+	
+		<div class="posts" id="posts">
+	
+			<?php
+			$paged = get_query_var( 'paged' ) ?: 1;
+			$total_post_count = wp_count_posts();
+			$published_post_count = $total_post_count->publish;
+			$total_pages = ceil( $published_post_count / $posts_per_page );
+			
+			if ( 1 < $paged ) : ?>
+			
+				<div class="page-title section small-padding">
+				
+					<h4 class="section-inner"><?php printf( __( 'Page %s of %s', 'hoffman' ), $paged, $wp_query->max_num_pages ); ?></h4>
+					
+				</div>
+				
+				<div class="clear"></div>
+			
+			<?php endif;
+			
+				while ( have_posts() ) : the_post();
+				
+					get_template_part( 'content', get_post_format() );
+					
+				endwhile;
+				
+			endif; ?>
+		
+	</div><!-- .posts -->
+	
+	<?php if ( $wp_query->max_num_pages > 1 ) : ?>
+	
+		<div class="archive-nav">
+		
+			<div class="section-inner">
+		
+				<?php echo get_next_posts_link( '&laquo; ' . __( 'Older posts', 'hoffman' ) ); ?>
+					
+				<?php echo get_previous_posts_link( __( 'Newer posts', 'hoffman' ) . ' &raquo;' ); ?>
+				
+				<div class="clear"></div>
+			
+			</div>
+			
+		</div><!-- .post-nav archive-nav -->
+						
+	<?php endif; ?>
+		
+</div><!-- .content section-inner -->
+	              	        
+<?php get_footer(); ?>
